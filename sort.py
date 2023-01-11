2574f40e3e0e0cb99b2a7a184fa336a629b4e5e1
Ardit <arditymeri7@gmail.com>
2023-01-11 14:37:51 +0000

    modified sort.py

--- sort.py
+++ sort.py
@@ -9,16 +9,18 @@ def main():
         #add a floating point number with at most 4 decimal places
         unsorted.append(random.uniform(0, 10000))
     f = open("unsorted.txt", "w")
+    f.write(str(1000000) + "\n")
     for i in unsorted:
         f.write(str(i) + " ")
     f.close()
 
-    #then sort the list and save it to a file called sorted.txt
-    unsorted.sort()
-    f = open("sorted.txt", "w")
-    for i in unsorted:
-        f.write(str(i) + " ")
-    f.close()
+    # #then sort the list and save it to a file called sorted.txt
+    # unsorted.sort()
+    # f = open("sorted.txt", "w")
+    # f.write(str(1000000) + "\n")
+    # for i in unsorted:
+    #     f.write(str(i) + " ")
+    # f.close()
 
 
 if __name__ == "__main__":
