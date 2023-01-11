3f108594bfacad052bc9617ceb7b2e0175bf52ef
Ardit <arditymeri7@gmail.com>
2023-01-11 14:41:40 +0000

    made sort.py dynamic

--- sort.py
+++ sort.py
@@ -1,15 +1,17 @@
+import sys
 def main():
     #generate unsorted list of floating point numbers, with size 1000000, and save it to a file called unsorted.txt
     #then sort the list and save it to a file called sorted.txt
 
-    #generate unsorted list of floating point numbers, with size 1000000, and save it to a file called unsorted.txt
+    #read size from command arguments
     import random
+    size = int(sys.argv[1])
     unsorted = []
-    for i in range(10_000_000):
+    for i in range(size):
         #add a floating point number with at most 4 decimal places
         unsorted.append(random.uniform(0, 10000))
     f = open("unsorted.txt", "w")
-    f.write(str(1000000) + "\n")
+    f.write(str(size) + "\n")
     for i in unsorted:
         f.write(str(i) + " ")
     f.close()
